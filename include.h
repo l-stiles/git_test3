36f3c454dcbf7205a5938b2197c751f68c77963f
l-stiles <z8438722@163.com>
2017-08-17 11:43:42 +0800

    master file

---
+++ include.h
@@ -0,0 +1,15 @@
+/*******************************************************************************
+****蓝旗嵌入式系统 STM8S105 EasyKit
+****LENCHIMCU.TAOBAO.COM
+****版本:V1.0
+****日期:14-2-2014
+****说明:主芯片STM8S105C4T6
+********************************************************************************/
+//包含函数:包含所有头文件
+#include "stm8s.h"
+#include "stm8s_it.h"
+#include "driver/uart.h"
+#include "driver/led.h"
+#include "driver/clock.h"
+#include "driver/timer1.h"
+#include "driver/cco.h"
